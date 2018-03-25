82612d8cc25d6872585bfe5d194810e16d38d2dd
kaleb <kaleb.wond@gmail.com>
2018-03-25 16:43:58 +0300

    mixed content https fixed

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URI', 'http://localhost'),
+    'url' => env('APP_URI', 'https://localhost'),
 
     /*
     |--------------------------------------------------------------------------
